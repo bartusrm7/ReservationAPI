25a5b6a1a9ce8e6858c220875a344e5a1689a5f1
Bartek <bbogucki7@gmail.com>
2026-01-05 18:24:07 +0100

    Changed db data

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -13,9 +13,9 @@ class Database
 
     public function __construct()
     {
-        $host = 'db4free.net';
-        $db = 'reservationapi';
-        $user = 'reservationapi';
+        $host = $_ENV['DB_HOST'];
+        $db = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USER'];
         $pass = $_ENV['DB_PASS'];
         $charset = 'utf8mb4';
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
