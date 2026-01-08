f0e2c901dc091e5b41589bf9e1e4cc0723837303
Bartek <bbogucki7@gmail.com>
2026-01-08 18:52:50 +0100

    Add railway db

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -13,12 +13,13 @@ class Database
 
     public function __construct()
     {
-        $host = $_ENV['DB_HOST'];
-        $db = $_ENV['DB_NAME'];
-        $user = $_ENV['DB_USER'];
-        $pass = $_ENV['DB_PASS'];
+        $host = getenv('DB_HOST');
+        $db   = getenv('DB_NAME');
+        $user = getenv('DB_USER');
+        $pass = getenv('DB_PASS');
+        $port = getenv('DB_PORT') ?: 3306;
         $charset = 'utf8mb4';
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
         $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false];
 
