3405b58f93fedfab42cd534d1d8b34480ebbce7e
Bartek <bbogucki7@gmail.com>
2026-01-09 08:56:35 +0100

    Updated Database.php

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -13,11 +13,16 @@ class Database
 
     public function __construct()
     {
-        $url = getenv('DATABASE_URL');
+        $host = getenv('DB_HOST');
+        $db = getenv('DB_NAME');
+        $user = getenv('DB_USER');
+        $pass = getenv('DB_PASS');
+        $port = 3306;
+        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
         $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false];
 
         try {
-            $this->pdo = new PDO($url, null, null, $options);
+            $this->pdo = new PDO($dsn, $user, $pass, $options);
         } catch (PDOException $err) {
             die('Database error: ' . $err->getMessage());
         }
